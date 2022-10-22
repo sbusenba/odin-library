b8af2700fe0d8dfc230af595760b2a6e76c0153a
Steven Busenbarrick <sbusenba@gmail.com>
2022-10-22 10:53:31 -0700

    install webpack and firebase move index to /dist.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBR9cH8iFBbQ5dR-4zjfEjPbh5M_uMblQE",
+  authDomain: "stevens-odin-library.firebaseapp.com",
+  projectId: "stevens-odin-library",
+  storageBucket: "stevens-odin-library.appspot.com",
+  messagingSenderId: "713537663214",
+  appId: "1:713537663214:web:bff2aa36df95f666e143ff"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 let myLibrary = []
 class Book{
     constructor(title,author,pages,read){
